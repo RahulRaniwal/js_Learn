65cd128e0ce4a9c2511c976f03aff8f59949efc1
Rahul Raniwal <59061631+RahulRaniwal@users.noreply.github.com>
2023-10-02 18:54:04 +0000

    Data Types

---
+++ 01Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";   // It will treat all js code as newer version
+
+// alert("Rahul");  // we are using node so we can't use it we can use it in browser
+
+let name = "Rahul"// STRING
+let age = 23
+let isLoggedIn = true
+
+// number => 2 topower 53
+// bigint
+// string
+// Boolean  
+// null => standalone value
+// undefined => value is not assigned
+//symbol => unique
+
+// object 
+
+console.log(typeof "Rahul");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof age);
