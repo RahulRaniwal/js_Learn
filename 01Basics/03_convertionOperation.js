1d963e80a9acd29b1d7c5a46223856bc5e0eb756
Rahul Raniwal <59061631+RahulRaniwal@users.noreply.github.com>
2023-10-02 19:07:48 +0000

    Convertion of data type

---
+++ 01Basics/03_convertionOperation.js
@@ -0,0 +1,25 @@
+let score = "69"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let num = Number(score);
+// console.log(typeof num);
+// console.log(num);
+
+// when we convert score to Number
+// "33" => 33
+// "33aaaa" => NaN
+// true => 1
+// null => 0
+
+let loggedIn = 1;
+let boolLoggedIn = Boolean(loggedIn)
+console.log(boolLoggedIn);
+console.log(typeof boolLoggedIn);
+
+// when we convert boolean
+
+// 1 => true, 0  => false
+// "" => false
+// "Rahul" => true
