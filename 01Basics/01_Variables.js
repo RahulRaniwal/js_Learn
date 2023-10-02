6a004538c0000257096eef82ad87310bfff231c3
Rahul Raniwal <59061631+RahulRaniwal@users.noreply.github.com>
2023-10-02 14:47:17 +0000

    variables

---
+++ 01Basics/01_Variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345666;
+let accountEmail = "rahul@google.com"
+var accountPass = "12345"
+accountCity = "Delhi"   // esa ho sakta he but esa nahi karna
+let variable;
+// accountId = 2232435454; // const can not be changed
+
+accountEmail  = "rr@gmail"
+accountPass = "2345"
+accountCity = "Rajasthan"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity , variable]);
+
+/*
+prefer not to use var
+dssds
+*/
