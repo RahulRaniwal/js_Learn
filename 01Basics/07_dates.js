9f23902e6969b9180e333c41cfe145066094a494
Rahul Raniwal <59061631+RahulRaniwal@users.noreply.github.com>
2023-10-04 14:19:57 +0000

    Date Done

---
+++ 01Basics/07_dates.js
@@ -0,0 +1,13 @@
+let date = new Date();
+
+// console.log(typeof date);
+// console.log(date);
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toJSON());
+// console.log(date.toString());
+// console.log(date.toLocaleTimeString());
+let myDate = new Date("02-21-2023")
+let newDate = Date.now()
+// console.log(myDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
